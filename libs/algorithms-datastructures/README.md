95ab4495762197f51ea50ef2893dce8bf57c7c92
Igor Cojocaru <igor.cojocaru@innovamat.com>
2023-10-02 00:01:30 +0200

    ADD configurations

---
+++ libs/algorithms-datastructures/README.md
@@ -0,0 +1,11 @@
+# algorithms-datastructures
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build algorithms-datastructures` to build the library.
+
+## Running unit tests
+
+`nx test algorithms-datastructures --watch`
---
+++ libs/algorithms-datastructures/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'algorithms-datastructures',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/algorithms-datastructures',
+};
